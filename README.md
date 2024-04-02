640e0466865716e77cb06f6c7b33e98800c730ee
jas387 <jhonatas387@gmail.com>
2024-04-02 15:23:34 -0300

    add audio,video,display,input,window,other and many things

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
 # A droidrec Flet app
+android record utility interface for scrcpy
 
-An example of a minimal Flet app.
+# How-To setup
+
+Create and change to project directory (DroidRec/src):
+
+```
+mkdir -pv DroidRec/src
+cd DroidRec
+```
+
+Clone this repo in src:
+
+```
+gh repo clone jas387/droidrec src/
+```
+
+Make virtual enviroment and activate:
+
+```
+python -m venv venv
+source venv/bin/activate
+```
+
+
+
+Install deps:
+
+```
+pip install -r src/requirements.txt
+```
 
 To run the app:
 
 ```
-flet run [app_directory]
+flet run src
 ```
--- main.py
+++ main.py
@@ -27,14 +27,14 @@ class TextEntry(flet.UserControl):
 
 
 class OptionList(flet.UserControl):
-    def __init__(self, label: str, options: list, default: str, *w, **kw):
+    def __init__(self, label: str, options: list, value: str, *w, **kw):
         super(OptionList,self).__init__(*w, **kw)
         self.label = label
         self.options = options
-        self.default = default
+        self.value = value
 
     def build(self):
-        return flet.Dropdown(label=self.label, options=[flet.dropdown.Option(opt) for opt in self.options], value=self.default)
+        return flet.Dropdown(label=self.label, options=[flet.dropdown.Option(opt) for opt in self.options], value=self.value)
 
 
 class CheckLists(flet.UserControl):
@@ -44,7 +44,7 @@ class CheckLists(flet.UserControl):
 
     def build(self):
         return flet.Row(controls=[
-                flet.Checkbox(label=label+":", value=value) for label, value in self._dict.items()
+                flet.Checkbox(label=label, value=value) for label, value in self._dict.items()
             ])
 
 
@@ -53,12 +53,12 @@ class Audio(flet.UserControl):
         super(Audio, self).__init__(*w, **kw)
         self.audio_bitrate = TextEntry(label='audio bitrate (suffix: K, M):', value='128K', text_type='custom', custom_filter=r'^[0-9]+[KMkm]*')
         self.audio_buffer = TextEntry(label='audio buffer (milliseconds):', text_type='number', value='50')
-        self.audio_codec_name = OptionList(label='audio codec name:', options=('opus', 'aac', 'flac', 'raw'), default='opus')
+        self.audio_codec_name = OptionList(label='audio codec name:', options=('opus', 'aac', 'flac', 'raw'), value='opus')
         self.audio_codec_options = TextEntry(label='audio codec options:', text_type='custom', custom_filter=r'^([A-Z_]+)\:(int|long|float|string)\=([A-Za-z0-9]+)', value='')
-        self.audio_encoder_name = OptionList(label='audio encoder name:', options=('get', 'from', '--list-encoders'), default='get')
-        self.audio_source = OptionList(label='audio source:', options=('output', 'mic'), default='output')
+        self.audio_encoder_name = OptionList(label='audio encoder name:', options=('get', 'from', '--list-encoders'), value='get')
+        self.audio_source = OptionList(label='audio source:', options=('output', 'mic'), value='output')
         self.audio_output_buffer = TextEntry(label='audio output buffer (milliseconds):', text_type='number', value='5')
-        self.audio_disable = CheckLists(_dict={'no audio': False, 'no audio playback': False, 'require audio': False})
+        self.audio_disable = CheckLists(_dict={'no audio': False, 'no audio playback': False, 'require audio': True})
 
     def build(self):
         return flet.Container(content=flet.Column(controls=[
@@ -72,6 +72,135 @@ class Audio(flet.UserControl):
                                 self.audio_disable,
                             ]), border=flet.border.all(3,flet.colors.BLUE), border_radius=flet.border_radius.all(10),padding=10,)
 
+class Video(flet.UserControl):
+    def __init__(self, *w, **kw):
+        super(Video, self).__init__(*w, **kw)
+        self.video_bitrate = TextEntry(label='video bitrate (suffix: K, M):', value='8M', text_type='custom', custom_filter=r'^[0-9]+[KMkm]*')
+        self.video_codec_name = OptionList(label='video codec name:', options=('h264', 'h265', 'av1'), value='h264')
+        self.video_codec_options = TextEntry(label='video codec options:', text_type='custom', custom_filter=r'^([A-Z_]+)\:(int|long|float|string)\=([A-Za-z0-9]+)', value='')
+        self.video_encoder_name = OptionList(label='video encoder name:', options=('get', 'from', '--list-encoders'), value='get')
+        self.video_source = OptionList(label='video source:', options=('display', 'camera'), value='display')
+        self.video_disable = CheckLists(_dict={'no video': False, 'no video playback': False, 'require video': True})
+        self.video_max_size = TextEntry(label='video max size:', value='0', text_type='number')
+        self.video_max_fps = TextEntry(label='video max fps:', value='', text_type='number')
+
+
+    def build(self):
+        return flet.Container(content=flet.Column(controls=[
+                                self.video_bitrate,
+                                self.video_codec_name,
+                                self.video_codec_options,
+                                self.video_encoder_name,
+                                self.video_source,
+                                self.video_max_size,
+                                self.video_disable,
+                            ]), border=flet.border.all(3,flet.colors.BLUE), border_radius=flet.border_radius.all(10),padding=10,)
+
+
+
+class Display(flet.UserControl):
+    def __init__(self, *w, **kw):
+        super(Display, self).__init__(*w, **kw)
+        self.display_buffer = TextEntry(label='display buffer (milliseconds):', value='0', text_type='number')
+        self.display_id = OptionList(label='display id:', value='0', options=('get', 'from', '--list-displays'))
+        self.display_orientation = OptionList(label='display orientation:', value='0', options='0 90 180 270 flip0 flip90 flip180 flip270'.split(' '))
+
+    def build(self):
+        return flet.Container(content=flet.Column(controls=[
+                self.display_buffer,
+                self.display_id,
+                self.display_orientation,
+            ]), border=flet.border.all(3,flet.colors.BLUE), border_radius=flet.border_radius.all(10),padding=10,)
+
+
+class Window(flet.UserControl):
+    def __init__(self, *w, **kw):
+        super(Window, self).__init__(*w, **kw)
+        self.window_title = TextEntry(label='window title:',text_type='alphanumeric', value='DroidRec')
+        self.window_x = TextEntry(label='window x:', value='auto', text_type='custom', custom_filter=r'^(auto|[0-9]+)$')
+        self.window_y = TextEntry(label='window y:', value='auto', text_type='custom', custom_filter=r'^(auto|[0-9]+)$')
+        self.window_width = TextEntry(label='window width (0 = auto):', value='0', text_type='number')
+        self.window_height = TextEntry(label='window height (0 = auto):', value='0', text_type='number')
+        self.window_options = CheckLists(_dict={'window borderless': False, 'always on top': False, 'fullscreen': False})
+        self.window_render_driver = OptionList(label='render driver:', value='opengles2',options=("direct3d", "opengl", "opengles2", "opengles", "metal", "software"))
+
+        
+    def build(self):
+        return flet.Container(content=flet.Column(controls=[
+                self.window_title,
+                self.window_x,
+                self.window_y,
+                self.window_width,
+                self.window_height,
+                self.window_options,
+            ]), border=flet.border.all(3,flet.colors.BLUE), border_radius=flet.border_radius.all(10),padding=10,)
+
+
+class V4L2(flet.UserControl):
+    def __init__(self, *w, **kw):
+        super(V4L2, self).__init__(*w, **kw)
+        self.v4l2_sink = TextEntry(label='v4l2 sink (/dev/videoN):', value='')
+        self.v4l2_buffer = TextEntry(label='v4l2 buffer(milliseconds):', value='0', text_type='number')
+
+    def build(self):
+        return flet.Container(content=flet.Column(controls=[
+                self.v4l2_sink,
+                self.v4l2_buffer,
+
+            ]), border=flet.border.all(3,flet.colors.BLUE), border_radius=flet.border_radius.all(10),padding=10,)
+
+
+class Record(flet.UserControl):
+    def __init__(self, *w, **kw):
+        super(Record, self).__init__(*w, **kw)
+        self.record_directory = TextEntry(label='record directory:', value='~/Videos')
+        self.record_filename = TextEntry(label='record filename:', value='{package}_{date}_{time}.{ext}')
+        self.record_format = OptionList(label='record format:', value='mkv', options=('mp4','mkv','m4a','mka','opus','aac','flac','wav'))
+        self.record_orientation = OptionList(label='record orientation:', value='0', options=('0','90','180','270'))
+
+        
+    def build(self):
+        return flet.Container(content=flet.Column(controls=[
+                self.record_directory,
+                self.record_filename,
+                self.record_format,
+                self.record_orientation,
+
+            ]), border=flet.border.all(3,flet.colors.BLUE), border_radius=flet.border_radius.all(10),padding=10,)
+
+
+class Input(flet.UserControl):
+    def __init__(self, *w, **kw):
+        super(Input, self).__init__(*w, **kw)
+        self.options = CheckLists(_dict={'otg': False, 'no keyboard': True, 'no mouse': True})
+        self.keyboard_mode = OptionList(label='keyboard mode', options=("disabled", "sdk", "uhid", "aoa"), value='uhid')
+        self.mouse_mode = OptionList(label='mouse mode', options=("disabled", "sdk", "uhid", "aoa"), value='uhid')
+
+
+    def build(self):
+        return flet.Container(content=flet.Column(controls=[
+                self.options,
+                self.keyboard_mode,
+                self.mouse_mode
+
+            ]), border=flet.border.all(3,flet.colors.BLUE), border_radius=flet.border_radius.all(10),padding=10,)
+
+
+
+class Other(flet.UserControl):
+    def __init__(self, *w, **kw):
+        super(Other, self).__init__(*w, **kw)
+        self.pause_on_exit = OptionList(label='pause on exit:', value='false', options=('true','false','if-error'))
+        self.check_buttons = CheckLists(_dict={'stay awake': False, 'version': False,'turn screen off': False, 'otg': False,'no control': True})
+        self.verbosity = OptionList(label='verbosity:',value='info', options=('verbose','debug','info','warn','error'))
+        
+    def build(self):
+        return flet.Container(content=flet.Column(controls=[
+                self.pause_on_exit,
+                self.check_buttons,
+                self.verbosity
+
+            ]), border=flet.border.all(3,flet.colors.BLUE), border_radius=flet.border_radius.all(10),padding=10,)
 
 
 class App:
@@ -91,13 +220,21 @@ class App:
         self.__build_ui()
 
     def __build_ui(self):
-        self.audio = Audio()
+        self.audio = flet.Tab(text='audio', content=Audio(), icon=flet.icons.AUDIO_FILE)
+        self.video = flet.Tab(text='video', content=Video(), icon=flet.icons.VIDEO_FILE)
+        self.display = flet.Tab(text='display', content=Display(), icon=flet.icons.DISPLAY_SETTINGS)
+        self.record = flet.Tab(text='record', content=Record(), icon=flet.icons.CAMERA)
+        self.window = flet.Tab(text='window', content=Window(), icon=flet.icons.WINDOW)
+        self.v4l2 = flet.Tab(text='v4l2', content=V4L2(), icon=flet.icons.VIDEOCAM)
+        self.input = flet.Tab(text='input', content=Input(), icon=flet.icons.KEYBOARD)
+        self.other = flet.Tab(text='other', content=Other(), icon=flet.icons.SETTINGS)
 
-        self.home = flet.Column(controls=[self.audio])
+        self.home = flet.Tabs(tabs=[self.audio, self.video, self.display, self.record, self.window, self.v4l2, self.input, self.other])
 
         self.page.controls.append(flet.SafeArea(content=self.home, expand=True))
         self.page.update()
 
+
 if __name__ == '__main__':
-    app = App()
+    app = App(width=800, height=600)
     flet.app(target=app.main)
